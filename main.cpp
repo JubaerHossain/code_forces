66945c09059c8beae49b38c13a0332c716704f28
JubaerHossain <jubaer01.cse@gmail.com>
2022-10-03 21:42:03 +0600

    g

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
 using namespace std; 
 
-int main()
-{
-    int w;
-    cin >> w;
-    if (w % 2 == 0 && w > 2)
-        cout << "YES";
-    else
-        cout << "NO";
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        bool ok = true;
+        for (int i = n - k; i < n; i++) {
+            if (a[i] != a[n - k]) {
+                ok = false;
+            }
+        }
+        if (ok) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
